406b85328735b83c0501b0f52cfc4030467b3c37
TeddyAgt <teddy.aboim@gmail.com>
2024-12-11 07:52:48 +0100

    added pokemons list on main view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon.model';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  standalone: false,
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    standalone: false,
+    styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'ng-first-project';
+export class AppComponent implements OnInit {
+    pokemons: Pokemon[] = POKEMONS;
+    selectedPokemon: Pokemon | undefined;
+
+    ngOnInit(): void {
+        console.table(this.pokemons);
+    }
+
+    selectPokemon(pokemonId: String) {
+        this.selectedPokemon = this.pokemons.find(
+            (pokemon) => pokemon.id === +pokemonId
+        );
+    }
 }
